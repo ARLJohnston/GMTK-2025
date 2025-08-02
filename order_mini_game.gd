c550172d2c691f2a16993d9978da41fd1ba9e0f3
BenjaminCochrane <ben@thesmallearth.co.uk>
2025-08-02 18:49:11 +0100

    Added arrow key assets and created the basic part of the order minigame with randomized key input puzzle and keys removed on press

--- order_mini_game.gd
+++ order_mini_game.gd
@@ -1,14 +1,47 @@
 extends Node
 
+var order = []
+var sprite_order = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	order = key_order_generator()
+	sprite_order = [$Key1, $Key2, $Key3, $Key4]
+	create_minigame_order()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _input(event) -> void:
+	if order.size() == 0:
+		queue_free()
+		return
+	var key_to_press = order[0]
+	# takes the front of order as the key that is to be pressed and then
+	# checks if a key is pressed and matches the key to be pressed, removeing that sprite from a list and 
+	if event.is_action_pressed("right_key"):
+		if key_to_press == "RIGHT" :
+			sprite_order.pop_front().hide()
+			order.pop_front()
+	if event.is_action_pressed("left_key"):
+		if key_to_press == "LEFT" :
+			sprite_order.pop_front().hide()
+			order.pop_front()
+	if event.is_action_pressed("up_key"):
+		if key_to_press == "UP" :
+			sprite_order.pop_front().hide()
+			order.pop_front()
+	if event.is_action_pressed("down_key"):
+		if key_to_press == "DOWN" :
+			sprite_order.pop_front().hide()
+			order.pop_front()
+		
+
+func create_minigame_order():
+	
+	change_sprite_texture($Key1, order[0])
+	change_sprite_texture($Key2, order[1])
+	change_sprite_texture($Key3, order[2])
+	change_sprite_texture($Key4, order[3])
+	
 
 func key_order_generator() -> Array:
 	var possible_actions = ["UP","DOWN","LEFT","RIGHT"]
@@ -19,11 +52,6 @@ func key_order_generator() -> Array:
 	
 	return order
 
-
-
 func change_sprite_texture(sprite,key):
-	var texture = load("res://assets/"+key+".svg")
+	var texture = load("res://assets/"+key+".png")
 	sprite.texture = texture
-	
-	
-	
