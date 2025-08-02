a0943d4a095ce8bbda24e9c72813bd9ade0736ed
interysy <2554268w@student.gla.ac.uk>
2025-08-02 17:38:32 +0100

    feat(coffee-maker-minigame): nozzle

--- coffee_maker_minigame.gd
+++ coffee_maker_minigame.gd
@@ -39,11 +39,20 @@ var winning_conditions = {
 		WinCondition.new(0.05, 0.10, CoffeeIngredient.Ingredient.MILK_FOAM)
 	]
 }
-
+func _ready() -> void: 
+	$Nozzle.hide() 
+	
+	var opaque_fill_style = StyleBoxFlat.new()
+	opaque_fill_style.bg_color = Color(1, 1, 1, 1)  # white and fully opaque
+	opaque_fill_style.set_corner_radius_all(5)
+	$FirstProgressBar.add_theme_stylebox_override("fill", opaque_fill_style)
+	
+	
 func _process(delta: float) -> void:    
 	
 	if (is_flowing):   
-		add_ingredient(delta, current_ingredient)
+		add_ingredient(delta, current_ingredient) 
+	
 	
 	if (self.layers.size() > 0 and is_flowing):   
 		var actual_ingredient_str = CoffeeIngredient.Ingredient.keys()[current_ingredient] 
@@ -52,12 +61,16 @@ func _process(delta: float) -> void:
 		match current_ingredient: 
 			CoffeeIngredient.Ingredient.COFFEE: 
 				current_bar.modulate = Color.SADDLE_BROWN  
+				$Nozzle.modulate = Color.SADDLE_BROWN 
 			CoffeeIngredient.Ingredient.MILK: 
-				current_bar.modulate = Color.MINT_CREAM 
+				current_bar.modulate = Color.MINT_CREAM  
+				$Nozzle.modulate = Color.MINT_CREAM 
 			CoffeeIngredient.Ingredient.MILK_FOAM: 
-				current_bar.modulate = Color.ANTIQUE_WHITE
+				current_bar.modulate = Color.ANTIQUE_WHITE 
+				$Nozzle.modulate = Color.ANTIQUE_WHITE 
 			CoffeeIngredient.Ingredient.CHOCOLATE: 
-				current_bar.modulate = Color.BROWN      
+				current_bar.modulate = Color.BROWN   
+				$Nozzle.modulate = Color.BROWN     
 				
 	const CHECK_THRESHOLD_LOWER = 90
 	const CHECK_THRESHOLD_UPPER = 110
@@ -141,37 +154,42 @@ func generate_new_bar():
 	new_bar.size = current_bar.size  
 	new_bar.rotation = -PI / 2  
 	new_bar.show_percentage = false  
+
+	# Transparent background (unfilled part)
 	var transparent_bg_style = StyleBoxFlat.new()
-	transparent_bg_style.bg_color = Color(0, 0, 0, 0) 
+	transparent_bg_style.bg_color = Color(0, 0, 0, 0)  # fully transparent
 	transparent_bg_style.set_corner_radius_all(5)
-
 	new_bar.add_theme_stylebox_override("background", transparent_bg_style)
-	
-	var percentage = layers[-1].percentage  / 100
-	var fractional_size = percentage * current_bar.size.y  
-	
-	
-	
-	const MARGIN = 4
 
-	new_bar.position = current_bar.position - Vector2(0, fractional_size * 2 + MARGIN)  
-	
+	# Opaque fill (filled part of the bar)
+	var opaque_fill_style = StyleBoxFlat.new()
+	opaque_fill_style.bg_color = Color(1, 1, 1, 1)  # white and fully opaque
+	opaque_fill_style.set_corner_radius_all(5)
+	new_bar.add_theme_stylebox_override("fill", opaque_fill_style)
+
+	var percentage = layers[-1].percentage / 100
+	var fractional_size = percentage * current_bar.size.y  
+
+	const MARGIN = 4
+	new_bar.position = current_bar.position - Vector2(0, fractional_size + 0.3 * fractional_size)  
+
 	current_bar.get_parent().add_child(new_bar) 
-	
 	current_bar = new_bar
+
 	
 func add_first_ingredient(ingredient : CoffeeIngredient.Ingredient): 
 	if first_ingredient == null:
 		first_ingredient = ingredient
-		layers.append(CoffeeIngredient.new(0, ingredient))  # Add a fresh layer
-		last_ingredient = ingredient  # Also update last_ingredient to match
+		layers.append(CoffeeIngredient.new(0, ingredient)) 
+		last_ingredient = ingredient  
 
 	
 func add_ingredient(delta: float , ingredient : CoffeeIngredient.Ingredient) -> void:  
 	const FLOW_RATE = 20
 	
 	if (last_ingredient == ingredient and layers.size() > 0):  		 
-		layers[-1].percentage += delta * FLOW_RATE  
+		layers[-1].percentage += delta * FLOW_RATE 
+		update_nozzle_color()  
 		print(layers[-1].percentage)
 	else: 
 		layers.append(CoffeeIngredient.new(ingredient, 0)) 
@@ -180,7 +198,30 @@ func add_ingredient(delta: float , ingredient : CoffeeIngredient.Ingredient) -> 
 	last_ingredient = ingredient
 		  
 		
-		
+func update_nozzle_color():    
+	if (is_flowing): 
+		$Nozzle.show()
+		$Nozzle.modulate = Color.SADDLE_BROWN 
+	else: 
+		$Nozzle.hide
+	 
+	
+	#var percentage = layers[-1].percentage  / 100
+	#var fractional_size = percentage * current_bar.size.y  
+	#var start_y = current_bar.position.y  
+	#
+	#var new_nozzle_y = start_y  + fractinal_size
+	#
+	##$Nozzle.position.y = start_y  + fractinal_size
+	
+	
+	 
+	
+	
+	
+	
+	
+	
 func ingredient_changed(new_ingredient : CoffeeIngredient.Ingredient): 
 	if (current_ingredient == null or current_ingredient != new_ingredient): 
 		generate_new_bar() 
@@ -212,15 +253,19 @@ func _on_milk_button_button_down() -> void:
 
 func _on_coffee_button_button_up() -> void:
 	is_flowing = false
+	$Nozzle.hide()
 
 
 func _on_chocolate_button_button_up() -> void:
 	is_flowing = false
+	$Nozzle.hide()
 
 
 func _on_foam_button_button_up() -> void:
-	is_flowing = false
+	is_flowing = false 
+	$Nozzle.hide()
 
 
 func _on_milk_button_button_up() -> void:
-	is_flowing = false
+	is_flowing = false 
+	$Nozzle.hide()
