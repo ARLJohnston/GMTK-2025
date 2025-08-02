077b81247396c40fc4c52e5a930c2a6d6958429d
ARLJohnston <github@arljohnston.com>
2025-08-02 18:23:53 +0100

    create basic order system

---
+++ order.gd
@@ -0,0 +1,57 @@
+class_name Order extends Sprite2D
+
+enum Orderable {
+	LATTE,
+	CAPPUCCINO,
+	MACCHIATO,
+	MOCHA,
+	DONUT,
+}
+
+const neons := [
+	Color(1.0, 0.0, 0.0),  # Neon Red
+	Color(0.0, 1.0, 0.0),  # Neon Green
+	Color(0.0, 0.0, 1.0),  # Neon Blue
+	Color(1.0, 1.0, 0.0),  # Neon Yellow
+	Color(1.0, 0.5, 0.0),  # Neon Orange
+	Color(0.5, 0.0, 0.5),  # Neon Purple
+	Color(0.0, 1.0, 1.0),  # Neon Cyan
+	Color(1.0, 0.0, 1.0),  # Neon Magenta
+	Color(0.5, 1.0, 0.5),  # Neon Light Green
+	Color(1.0, 0.5, 1.0),  # Neon Light Pink
+]
+
+var order := {}
+
+func _init(difficulty: int = 3):
+	for o in Orderable.keys():
+		order[o] = 0
+		
+	for i in range(difficulty):
+		var item = Orderable.keys()[randi() % Orderable.keys().size()]
+		order[item] += 1
+		
+	self_modulate = neons[randi()%neons.size()]
+	var text := "Order:"
+	for o in order.keys():
+		if order[o] == 1:
+			text += "\n\t- " + o
+		if order[o] > 1:
+			text += "\n\t- %d %ss" % [order[o], o]
+			
+	var label := Label.new()
+	label.position = Vector2(50, 10)
+	label.modulate = Color.BLACK
+	label.text = text
+	add_child(label)
+
+func check_order(inventory: Array) -> Array:
+	var new_inventory = inventory
+	
+	for obj in order:
+		if obj not in new_inventory:
+			return [false, inventory]
+		new_inventory.erase(obj)
+		
+	self.queue_free()
+	return [true, new_inventory]
